3ffb67e1a5fbb79cedc5aea809ef24def34e3470
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-07-21 16:27:03 +0400

    initial commit

---
+++ src/application.js
@@ -0,0 +1,5 @@
+// @ts-check
+
+// BEGIN (write your solution here) (write your solution here)
+
+// END
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import app from './application.js';
+
+const laptops = [
+  {
+    model: 'v1', processor: 'intel', frequency: 1.7, memory: 16,
+  },
+  {
+    model: 'd3', processor: 'intel', frequency: 3.5, memory: 8,
+  },
+  {
+    model: 'd2', processor: 'amd', frequency: 2.5, memory: 16,
+  },
+];
+
+app(laptops);
